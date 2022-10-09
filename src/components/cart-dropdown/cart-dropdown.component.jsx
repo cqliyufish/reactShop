b530348cce3710143c7e69fbccd7d0217013a9fa
YU Li <liyu.cq88@gmail.com>
2022-10-08 21:06:43 -0700

    finish checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,19 @@
 import "./cart-dropdown.styles.scss";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "components/button/button.component";
 import CartItem from "components/cart-item/cart-item.component";
 
 import { CartContext } from "context/cart.context";
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-
+  //1. import useNavigate from Router
+  //2. checkout page跳转
+  const navigate = useNavigate();
+  // 3. 定义onClick事件函数
+  const goToCheckOutHandle = () => {
+    navigate("/checkout");
+  };
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -14,7 +21,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckOutHandle}>CHECKOUT</Button>
     </div>
   );
 };
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -2,6 +2,7 @@ import { createContext, useState, useEffect } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   //if already exists, increase quantity
+
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
   );
@@ -18,22 +19,58 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToDel) => {
+  //find the cart item
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToDel.id
+  );
+  //if quantity == 1, remove item from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToDel.id);
+  } else {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToDel.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+  }
+};
+
+const clearCartItem = (cartItems, cartItemToDel) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToDel.id);
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
-
+  const [cartTotal, setCartTotal] = useState(0);
+  //添加item
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
+
+  //删除item数量
+  const removeItemFromCart = (cartItemToDel) => {
+    setCartItems(removeCartItem(cartItems, cartItemToDel));
+  };
+
+  //clear item
+  const clearItemFromCart = (cartItemToDel) => {
+    setCartItems(clearCartItem(cartItems, cartItemToDel));
+  };
+
   // cartItems change, recount the number of items
   useEffect(() => {
     const newCartCount = cartItems.reduce(
@@ -43,12 +80,24 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount);
   }, [cartItems]);
 
+  // cartItems change, recount the total
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemFromCart,
+    clearItemFromCart,
     cartItems,
     cartCount,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
